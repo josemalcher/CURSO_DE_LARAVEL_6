ec0ea4f6e64e7a286907ff4dd57923018e431202
José Stélio Malcher Jr <contato@josemalcher.net>
2020-07-10 06:19:52 -0300

    35 - Validações no Laravel 6

--- laravel-repositories/app/Http/Controllers/ProductController.php
+++ laravel-repositories/app/Http/Controllers/ProductController.php
@@ -55,6 +55,14 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
+
+        $request->validate([
+            'name'          => 'required | min:3 | max: 255',
+            'description'   => 'nullable | min:3 | max:10000',
+            'photo'         => 'required | image'
+        ]);
+
+
         //dd($request->all());
         /*
          array:3 [▼
--- laravel-repositories/resources/views/admin/pages/products/create.blade.php
+++ laravel-repositories/resources/views/admin/pages/products/create.blade.php
@@ -4,6 +4,17 @@
 
 @section('content')
     <h1>Cadastrar novo produto</h1>
+
+    @if($errors->any())
+
+    <ul>
+        @foreach($errors->all() as $error)
+            <li>{{$error}}</li>
+        @endforeach
+    </ul>
+
+    @endif
+
     <form action="{{route('products.store')}}" method="POST" enctype="multipart/form-data">
         @csrf
         <input type="text" name="name" id="name" placeholder="Nome: ">
